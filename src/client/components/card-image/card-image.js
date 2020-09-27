c357d72b101aad9d47331034603b06332fdc9dee
Ambuj <Ambuj.Gupta@walmartlabs.com>
2020-09-28 02:33:20 +0530

    CSS corrected

--- src/client/components/card-image/card-image.js
+++ src/client/components/card-image/card-image.js
@@ -5,7 +5,7 @@ import '../styles.css';
 const CardImage = (props) => {
     return (
         <div>
-            <div className="imageContainer">
+            <div className="imageContainer fr jtc">
                 <img src={props.imageLink} alt="Denim Jeans" className="cardImage" />
             </div>
             <h3 className="fs_16 imageTitle " style={{ maxWidth: 'fit-content' }}>{`${props.missionName}#${props.flightNumber}`}</h3>
